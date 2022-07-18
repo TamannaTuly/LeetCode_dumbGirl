a8415aa8fb358ac5de08370dee0a441e6610d0b8
tamanna.tuly <ttniger53@gmail.com>
2022-07-18 18:39:47 +0600

    9. Palindrome Number

--- Leetcode version easy 1/9.Palindrome_Number.py
+++ Leetcode version easy 1/9.Palindrome_Number.py
@@ -1,2 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        temp = x
+        rest = 0
+        while x > 0:
+            foot_value = x % 10
+            rest = rest * 10 + foot_value
+            x = x // 10
+
+        if temp == rest:
+            return True
+        else:
+            return False
