07d2d2046f663ee68d76daa7f01b7032c7707585
tamanna.tuly <ttniger53@gmail.com>
2022-07-19 18:53:07 +0600

    13. roman to int convert

--- Leetcode version easy 1/13.Roman_to_Integer.py
+++ Leetcode version easy 1/13.Roman_to_Integer.py
@@ -1,17 +1,10 @@
-I = 1
-V = 5
-X = 10
-L = 50
-C = 100
-D = 500
-M = 1000
-IV = 4
-XL = 40
-CD = 400
-IX = 9
-XC = 90
-CM = 900
-
-s = input(str())
-
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_value =  {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        initial_value = 0
+        for i in range (len(s)):
+            if i>0 and roman_value[s[i]]>roman_value[s[i-1]]:
+                initial_value += roman_value[s[i]]-2*roman_value[s[i-1]]
+            else:
+                initial_value += roman_value[s[i]]
+        return initial_value
