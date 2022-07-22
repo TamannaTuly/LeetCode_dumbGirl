3b158118b85cbf9c1be35884221ed9013b27934b
tamanna.tuly <ttniger53@gmail.com>
2022-07-22 11:32:54 +0600

    21. Merge two sorted list - practice updated

--- Leetcode version easy 1/practice merge sort list.py
+++ Leetcode version easy 1/practice merge sort list.py
@@ -1,6 +1,22 @@
-list1 = []
+list1 = [1,2]
 list2 = [0]
 
-list3 = sorted(list2+list1)
+# one way of sorting:
+# list3 = sorted(list2+list1)
+#
+# print(list3)
 
-print(list3)
+
+# second way:
+res = []
+i = j = 0
+
+while i<len(list1) and j<len(list2):
+    if list1[i]<list2[j]:
+        res.append(list1[i])
+        i += 1
+    else:
+        res.append(list2[j])
+        j += 1
+res = res + list1[i:] + list2[j:]
+print(res)
