c7c35e11b143c4d3d8d4e94209ded444c6ad299d
tamanna.tuly <ttniger53@gmail.com>
2022-07-24 10:48:01 +0600

    Random practice

---
+++ Practice Set/practice_random.py
@@ -0,0 +1,16 @@
+# x = 3e3
+# print(x)
+# print(type(x))
+
+x = 3+5j
+y = 5j
+z = -5j
+print(x)
+print(y)
+print(z)
+
+a = 3
+b = complex(a)
+print(b)
+
+# Note: You cannot convert complex numbers into another number type.
