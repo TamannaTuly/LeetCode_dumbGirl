387517c5d173dd06d294506c9ff855d416faf714
tamanna.tuly <ttniger53@gmail.com>
2022-07-24 12:03:09 +0600

    Random practice

--- Practice Set/practice_random.py
+++ Practice Set/practice_random.py
@@ -2,15 +2,105 @@
 # print(x)
 # print(type(x))
 
-x = 3+5j
-y = 5j
-z = -5j
-print(x)
-print(y)
-print(z)
-
-a = 3
-b = complex(a)
-print(b)
+# / x = 3+5j
+# y = 5j
+# z = -5j
+# print(x)
+# print(y)
+# print(z)
+#
+# a = 3
+# b = complex(a)
+# print(b)/
 
 # Note: You cannot convert complex numbers into another number type.
+
+
+# random number generation:
+
+# /import random
+# randomNumber = random.randrange(1, 9)
+# print(randomNumber)/
+
+
+# multiline strings :
+
+# /multilineString = """hi,
+# dear tuly.
+# hope you are doing well. let's meet after work today
+# yours maliha"""
+#
+# print(multilineString)/
+
+
+# Strings are Arrays :
+# /stringArray = "apple"
+# print(stringArray[4]) # will print word of position 4/
+
+# Looping Through a String :
+# /loopString = 'testerbird'
+# for x in loopString:
+#     print(x)/
+
+
+# Check String :
+# /test = '''hi
+# there i am tuly speaking, how may i help you?'''
+#
+# print('y' in test)/
+
+
+# Use it in an if statement:
+
+# /test = 'hello you, who are you?'
+#
+# if 'who' in test:
+#     print('present')/
+
+# Check not in String :
+# /test = '''hi
+# there i am tuly speaking, how may i help you?'''
+#
+# print('x' not in test)/
+
+# Use it in an if statement:
+
+# /test = 'hello you, who are you?'
+#
+# if 'who' not in test:
+#     print('present')
+# else:
+#     print('absent')/
+
+# Slicing Strings :
+# Get the characters from position 2 to position 5 :
+
+# sliceable_string = 'i am from bangladesh'
+# print(sliceable_string[2:6]) #here last length == 6-1 == 5 which means last postion - 1 will be returned WHERE INDEx starting at psotion 0
+
+
+# Slice From the Start :
+
+# sliceable_string = 'test bird is nothing but a tester word'
+#
+# print(sliceable_string[:4]) # print up to position 3 as 4-1 == 3
+
+
+# Slice To the End :
+# /sliceable_string = 'bangladesh my bangladesh '
+# print(sliceable_string[9:]) # will start from position 9/
+
+
+# Negative Indexing :
+# sliceable_string = 'bangladesh my bangladesh'
+# print(sliceable_string[-3: ])
+
+
+
+
+
+
+
+
+
+
