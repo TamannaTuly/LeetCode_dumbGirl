6e7735c1f2b1e9575f8fa7975cb43a33e5295cc0
tamanna.tuly <ttniger53@gmail.com>
2022-07-24 10:39:04 +0600

    1480. Running Sum of 1d Array

--- Leetcode version easy 1/1480. Running Sum of 1d Array.py
+++ Leetcode version easy 1/1480. Running Sum of 1d Array.py
@@ -1,18 +1,11 @@
 #practice :
 #given
-nums = [1, 2, 3, 4]
-# res = []
-# i = 0
-# for i in range(len(nums)):
-#     sum = nums[i]
-#     returnSum = res[i]
-#     test = sum+returnSum
-#     print(sum)
-#     print(test)
-#     res.append(test)
-# print(res)
+nums = [1, 2, 3, 9]
 
+# solution 1 :
 for i in range(1, len(nums)):
     print(i)
     nums[i] += nums[i-1]
 print(nums)
+
+
